f38a214fad0953502d16b153547592b451e7c79d
herlina <herlinabjb46@gmail.com>
2017-10-25 04:47:04 +1100

    biaya parket

--- praktek0204.cpp
+++ praktek0204.cpp
@@ -12,4 +12,8 @@ int main(){
 	
 	JumlahBoxP = LuasRumah/Luas1Box;
 	cout<<"Jumlah Box Parket		:"<<JumlahBoxP<<endl;
+	
+	BiayaP = JumlahBoxP*500;
+	cout <<"Biaya Parket (Rp)		: "<<BiayaP<<endl;
+	
 }
